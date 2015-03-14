3380a0069756e8705d73518504ab717d8d6f7f79
Helena <helena.talimaa@gmail.com>
2015-03-14 13:10:59 +0200

    Muudatused

--- src/Mängulaud.java
+++ src/Mängulaud.java
@@ -2,23 +2,46 @@
  * Created by Helena on 13/03/2015.
  */
 import java.util.ArrayList;
+import java.util.Random;
+
 public class Mängulaud {
     private ArrayList<Integer> ruudud;
+    private ArrayList<Integer> arvuti_käigud;
     private ArrayList<String> käigud;
-    //private String laud;
+    Random juhuslikkus = new Random();
 
-    public Mängulaud(ArrayList<Integer> ruudud, ArrayList<String> käigud) {
+    public Mängulaud(ArrayList<Integer> ruudud, ArrayList<Integer> arvuti_käigud, ArrayList<String> käigud) {
         this.ruudud = ruudud;
+        this.arvuti_käigud = arvuti_käigud;
         this.käigud = käigud;
     }
 
-    public ArrayList<String> getKäigud() {
+     public ArrayList<String> getKäigud() {
         return käigud;
     }
 
     public void setKäigud(ArrayList<String> käigud) {
         this.käigud = käigud;
     }
+
+    int teeKaik() {
+        for (int i =0; i < 9; i++) {
+            if (käigud.get(i).equals("X") || käigud.get(i).equals("O")) {
+                if(arvuti_käigud.contains(i)) {
+                    arvuti_käigud.remove(i);
+                }
+            }
+        }
+
+        System.out.println(arvuti_käigud);
+        int number = juhuslikkus.nextInt(arvuti_käigud.size());
+        System.out.println(number);
+        System.out.println(arvuti_käigud.get(number));
+        return arvuti_käigud.get(number);
+
+    }
+
+
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
--- src/TripsTrapsTrull.java
+++ src/TripsTrapsTrull.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class TripsTrapsTrull {
 
+
     public static void main(String[] args) throws Exception {
 
         System.out.println("Tere tulemast mängima Trips-Traps-Trulli!");
@@ -14,13 +15,15 @@ public class TripsTrapsTrull {
         //boolean mängu_jätk = true;
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> järjend = new ArrayList<Integer>();
+        ArrayList<Integer> arvuti_käigud = new ArrayList<Integer>();
         ArrayList<String> käigud = new ArrayList<String>();
         for (int a =0; a < 9; a++) {
             järjend.add(a);
             käigud.add("");
+            arvuti_käigud.add(a);
         }
 
-        Mängulaud a = new Mängulaud(järjend, käigud);
+        Mängulaud a = new Mängulaud(järjend, arvuti_käigud, käigud);
 
         while (true) {
             System.out.println(a);
@@ -35,6 +38,10 @@ public class TripsTrapsTrull {
             else {
                 käigud.set(number, "X");
                 a.setKäigud(käigud);
+                int arvuti_number = a.teeKaik();
+                käigud.set(arvuti_number, "O");
+                a.setKäigud(käigud);
+
             }
         }
 
